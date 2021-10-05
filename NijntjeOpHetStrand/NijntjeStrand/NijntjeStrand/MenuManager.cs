d6cb603cedbbc66b4de08dd618b5cf27525f62c0
Marco Boschma <marcoboschma@live.nl>
2021-10-05 15:10:36 +0200

    Menu Maneger aangemaakt. De manager bevat: Publieke namen voor speler 1 & 2 Een method om een string te veranderen met een overload om de speler namen direct te veranderen.

---
+++ NijntjeOpHetStrand/NijntjeStrand/NijntjeStrand/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NijntjeStrand
+{
+
+    //regelt het laden van volgende windows
+    class MenuManager
+    {
+
+
+        public string namePlayer1 = "Voer naam speler 1 in";            //Naam van speler 1
+        public string namePlayer2 = "Voer naam speler 2 in";            //Naam van speler 2
+
+        public static string namePlayer_1 = "";                         //publieke naam speler 1
+        public static string namePlayer_2 = "";                         //publieke naam speler 2
+
+        //functie voor het veranderen van een naam
+        public static string SetSpelerNaam(string input)
+        {
+
+            return input;
+        }
+        //functie voor het veranderen van een speler naam
+        public static string SetSpelerNaam(int Speler, string Naam)
+        {
+            //als er 1 word ingevoerd
+            if(Speler == 1)
+            {
+                namePlayer_1 = Naam;                                    //zet speler naam
+                return "naam gezet";
+            }
+            if(Speler == 2)
+            {
+                namePlayer_2 = Naam;                                    //zet speler naam
+                return "naam gezet";
+            }
+            return "geen geldige speler";                               //Geen geldige speler geselecteerd
+        }
+
+    }
+}
